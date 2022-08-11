34e286d69641f339738130cdda31f7ce7d19eef6
cxmarcelo <marcelo.c.b22@gmail.com>
2022-08-11 14:46:09 -0300

    Configurações no servidor git

--- fms-oauth/src/main/java/br/com/trescon/fmsoauth/config/AuthSecurityConfig.java
+++ fms-oauth/src/main/java/br/com/trescon/fmsoauth/config/AuthSecurityConfig.java
@@ -17,10 +17,13 @@ import org.springframework.security.config.annotation.web.configuration.OAuth2Au
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -102,7 +105,7 @@ public class AuthSecurityConfig {
 	
 	@Bean
 	JwtEncoder jwtEncoder() {
-		return new NimbusJwtEncoder(null)
+		return new NimbusJwtEncoder(null);
 	}
 	
 }
